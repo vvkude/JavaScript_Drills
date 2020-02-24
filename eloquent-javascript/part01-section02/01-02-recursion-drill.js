3b6136ed143fc4032288920b0450b3240477ef54
Vik Kudesia <vvkudesia@gmail.com>
2020-02-24 00:03:40 -0500

    [FIX] Replace Math.abs with a recursive call with -n so that we don't blow the stack

--- eloquent-javascript/part01-section02/01-02-recursion-drill.js
+++ eloquent-javascript/part01-section02/01-02-recursion-drill.js
@@ -1,7 +1,9 @@
 function isEven(n) {
     if (n == 0) return true;
     else if (n == 1) return false;
-    else if (n < 0) return isEven(Math.abs(n));
+    // We shouldn't know about Math.abs, yet. So, we check for negative
+    // numbers and, if we find one, recursively call isEven() with -n
+    else if (n < 0) return isEven((-n));
     // A mathematical definition of evenness in numbers includes
     // that a number, n, has the same evenness as n-2
     else return isEven(n-2));
