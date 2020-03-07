8f10bc40744183a9ca4dedc2602854cb4f11b037
Vik Kudesia <vvkudesia@gmail.com>
2020-03-07 18:01:25 -0500

    [ADD] trinary operator decides on direction of step

--- eloquent-javascript/part01-section04/sum-of-range.js
+++ eloquent-javascript/part01-section04/sum-of-range.js
@@ -1,12 +1,15 @@
 // snippet is for eloquent javasript part 01 chapter 04
 // write a function that accepts start, end and step value that creates
 // an array of those numbers, inclusive and increases by the step
+// Can't use higher order functions like Array.of
 
-let vals = [];
-function range (start, end, step = 1) {
-	for (let i = 0; i < end; i++) {
-		vals.push(start + i)
+
+function range(start, end, step = start < end ? 1 : -1) {
+	let array = [];
+	if (step > 0) {
+		for (let i = start; i <= end; i += step) array.push(i);
+	} else {
+		for (let i = start; i >= end; i += step) array.push(i);
 	}
-	return vals;
+	return array;
 }
-
