a6ebc90d1425c218ef65fd01a75d68741456f4ea
Vik Kudesia <vvkudesia@gmail.com>
2020-03-08 23:38:15 -0400

    [ADD] Set list value to null by default

---
+++ eloquent-javascript/part01-section04/arrays-to-lists.js
@@ -0,0 +1,15 @@
+/* Eloquent Javascript Part 01 Chapter 04 Exercise 03
+ * Create a function that creates a list from an array
+ * Create a function that creates arrays from a list
+ * Create helper functions that prepend lists
+*/
+
+// Try looping backwards like in the last exercise
+
+function arrayToList(array) {
+    let list = null;
+    for (i = array.length - 1; i > 0; i--) {
+        list = {value: array[i], rest: list};
+    }
+    return list;
+}
