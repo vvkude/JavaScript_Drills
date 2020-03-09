d797d900fb1fbe3af9a68ed2a12c20d08bd8c6e0
Vik Kudesia <vvkudesia@gmail.com>
2020-03-08 23:42:06 -0400

    [FIX] Previously, we were not looping enough times because we did not loop until i was less than or equal to zero

--- eloquent-javascript/part01-section04/arrays-to-lists.js
+++ eloquent-javascript/part01-section04/arrays-to-lists.js
@@ -8,7 +8,7 @@
 
 function arrayToList(array) {
     let list = null;
-    for (i = array.length - 1; i > 0; i--) {
+    for (i = array.length - 1; i >= 0; i--) {
         list = {value: array[i], rest: list};
     }
     return list;
